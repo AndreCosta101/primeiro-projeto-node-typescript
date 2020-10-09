1354bd5fc094b636679ce04f2f3a14c58b40c650
AndreCosta101 <andre.costa101@gmail.com>
2020-10-09 00:54:26 -0300

    Relacionamento nos models

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -5,7 +5,17 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+
+import User from './User';
+
+/**
+ * Um para Um (One to One)
+ * Um para Muitos (One to Many)
+ * Muitos para Muitos (Many to Many)
+ */
 
 @Entity('appointments')
 class Appointment {
@@ -13,7 +23,12 @@ class Appointment {
   id: string;
 
   @Column() // passando vazio, ele usa varchar
-  provider: string;
+  provider_id: string;
+
+  // a referência do relacionamento é o Appointment, portanto ManyToOne
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
