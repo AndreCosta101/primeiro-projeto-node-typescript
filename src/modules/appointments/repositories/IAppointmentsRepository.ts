8ef5588a2f9d89ebb6e6f9d3872ad8d86e0bc4ca
AndreCosta101 <andre.costa101@gmail.com>
2020-10-24 13:35:10 -0300

    Reescrevendo Repositórios

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
+import ICreateAppointmentDto from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDto): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
