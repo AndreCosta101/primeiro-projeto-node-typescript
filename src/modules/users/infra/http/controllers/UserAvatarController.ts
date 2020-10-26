744433bd5e2c4a8005c8d0fef0bd7417f5f7d86c
AndreCosta101 <andre.costa101@gmail.com>
2020-10-26 02:39:23 -0300

    Usando controllers

---
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+export default class UserAvatarController {
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
+
+    // request.user.id foi colocado no middleware
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    return response.json(user);
+  }
+}
