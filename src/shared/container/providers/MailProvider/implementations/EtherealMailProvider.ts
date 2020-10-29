707806f7c62c6e814d9e849f32da2088c374fb74
AndreCosta101 <andre.costa101@gmail.com>
2020-10-29 16:16:15 -0300

    Emails em desenvolvimento

---
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,34 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import ImailProvider from '../models/IMailProvider';
+
+export default class EtherealMailProvider implements ImailProvider {
+  private client: Transporter;
+
+  constructor() {
+    nodemailer.createTestAccount().then(account => {
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure, // true for 465, false for other ports
+        auth: {
+          user: account.user, // generated ethereal user
+          pass: account.pass, // generated ethereal password
+        },
+      });
+
+      this.client = transporter;
+    });
+  }
+
+  public async sendMail(to: string, body: string): Promise<void> {
+    const message = await this.client.sendMail({
+      from: 'Equipe GoBarber <equipegobarber@gobarber.com>',
+      to,
+      subject: 'Yipee ka-yay motherfucker',
+      text: body,
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
