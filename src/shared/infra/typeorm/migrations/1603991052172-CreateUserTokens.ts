d054c172887176985662bb647137a4ddd118b1ca
AndreCosta101 <andre.costa101@gmail.com>
2020-10-29 15:23:37 -0300

    Salvando tokens no banco

--- src/shared/infra/typeorm/migrations/1603991052172-CreateUserTokens.ts
+++ src/shared/infra/typeorm/migrations/1603991052172-CreateUserTokens.ts
@@ -1,11 +1,56 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+/* eslint-disable prettier/prettier */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUserTokens1603991052172 implements MigrationInterface {
-
+export default class CreateUserToken1603977264996
+    implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'user_tokens',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'token',
+                        type: 'uuid',
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'user_id',
+                        type: 'uuid',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        name: 'TokenUser',
+                        referencedTableName: 'users',
+                        referencedColumnNames: ['id'],
+                        columnNames: ['user_id'],
+                        onDelete: 'CASCADE',
+                        onUpdate: 'CASCADE',
+                    },
+                ],
+            }),
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('user_tokens');
     }
-
 }
