9f04616a1393b65e13d84f3b0c6311a53ad74ba0
AndreCosta101 <andre.costa101@gmail.com>
2020-10-31 13:41:23 -0300

    Excluindo horarios antigos

--- src/modules/appointments/services/DayAvailabilityService.spec.ts
+++ src/modules/appointments/services/DayAvailabilityService.spec.ts
@@ -15,12 +15,16 @@ describe('ListProviderDayAvailability', () => {
   it('should be able to check the providers day availability', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 10, 20, 8, 0, 0),
+      date: new Date(2020, 10, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 10, 20, 10, 0, 0),
+      date: new Date(2020, 10, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 10, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -30,12 +34,15 @@ describe('ListProviderDayAvailability', () => {
       day: 20,
     });
 
-    expect(availability).toEqual(
+    await expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
